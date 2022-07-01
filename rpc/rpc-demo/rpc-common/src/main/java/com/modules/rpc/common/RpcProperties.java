79a3beb9aee3226661fe43b7d4e1cdb6b278b868
chenlingl <chenlingl@4px.com>
2022-07-01 17:25:52 +0800

    Desc:增加rpc功能的实现！

---
+++ rpc/rpc-demo/rpc-common/src/main/java/com/modules/rpc/common/RpcProperties.java
@@ -0,0 +1,16 @@
+package com.modules.rpc.common;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "rpc")
+public class RpcProperties {
+
+    private Integer servicePort;
+
+    private String registryAddr;
+
+    private String registryType;
+
+}
